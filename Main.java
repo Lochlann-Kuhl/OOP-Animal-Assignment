84bd41d1251d7da705ae6962df3c500336b33cf5
kuhll0055 <kuhll0055@gmail.com>
2022-05-12 08:59:34 -0400

    added some print statements to main

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
 public class Main {
     public static void main(String[] args){
+        // testing --> to be deleted
         int lad;
         int lad2 = 5;
         Capricornus tommy = new Capricornus() ;
         lad = tommy.CapAgility(lad2);
-        System.out.print(lad);
+        System.out.println(lad);
+        // beginning of actual code
+        String[] constellationNames = {
+                "Aquila","Astra","Cancer","Canis Major","Capricornus","Leo","Lyra","Pisces","Scorpius"
+        };
+        System.out.println("Your choice include: ");
+        for (int i =0; i < constellationNames.length; i++) {
+            if (i == (constellationNames.length)-1) {
+                System.out.println("and " + constellationNames[i] + ".");
+            } else {
+                System.out.print(constellationNames[i] + ", ");
+            }
+        }
     }
 }
