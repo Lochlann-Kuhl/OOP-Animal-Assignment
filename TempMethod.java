61a4830204ab4cdd23b3420bd21e5f946313637c
ObsidianGator <emanhals14@wrdsb.ca>
2022-05-19 09:36:05 -0400

    Beginning the ai stat generation

--- TempMethod.java
+++ TempMethod.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class TempMethod {
-
+    public static int[] aiStats (ArrayList<String> envList) {
+        int i =0;
+        int aiStatPool;
+        while(i = 7 && aiStatPool > 0;){
+            int[] aiStat = new int[7];
+            aiStat[i] = (int)(Math.random()*4);
+            i++;
+        }
+    }
 }
