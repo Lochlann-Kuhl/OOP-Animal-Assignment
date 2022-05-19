f40c726cfcd9703d52cb85b7965918f07e9f9595
kuhll0055 <kuhll0055@gmail.com>
2022-05-19 09:07:29 -0400

    created an environment select method that will be later transferred to main

--- Environment.java
+++ Environment.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 abstract class Environment implements Defined_Environment_Classes{
     public double spdWeight() { return 1; }
 
@@ -12,5 +14,16 @@ abstract class Environment implements Defined_Environment_Classes{
     public double litWeight() { return 1; }
 
     public double edrWeight() { return 1; }
+
+    // randomly generates an environment from an arraylist
+    // removes that environment from the arraylist, so it cannot be repeated
+    public static String envGen (ArrayList<String> envList) {
+        String selectEnv;
+        int iSelectEnv;
+        iSelectEnv = (int)(Math.random()*8);
+        selectEnv = envList.get(iSelectEnv);
+        envList.remove(selectEnv);
+        return selectEnv;
+    }
 
 }
