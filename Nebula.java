094f5ce598fd35b3725b58bf9cefff1faebe0e20
ObsidianGator <emanhals14@gmail.com>
2022-05-23 01:58:50 -0400

    fixed up the compare method and aiStats methods, as well as added difficulties

--- Nebula.java
+++ Nebula.java
@@ -1,17 +1,11 @@
 public class Nebula extends Environment {
     public double spdWeight() { return 0.5; }
 
-    public double aglWeight() {
-        return 2;
-    }
+    public double aglWeight() {return 2; }
 
-    public double durWeight() {
-        return 1;
-    }
+    public double durWeight() {return 1; }
 
-    public double atkWeight() {
-        return 1;
-    }
+    public double atkWeight() {return 1; }
 
     public double grvWeight() { return 1; }
 
